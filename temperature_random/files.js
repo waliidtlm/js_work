6063c8269fe4ab5eea0f31df7bed7cbf595037f0
walidtlm <tlemcani.waliid@gmail.com>
2023-09-20 23:31:48 +0100

    update

--- temperature_random/files.js
+++ temperature_random/files.js
@@ -1,26 +1,39 @@
 const fs = require('fs');
-const readline = require ('readline');
+const readline = require('readline');
 
 const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout,
-})
+  input: process.stdin,
+  output: process.stdout,
+});
+
 let cities;
-function func () {
-    fs.readFile('input.txt', (err, data)=>{
-        cities = JSON.parse(data);
-        console.log(cities)
-    })
-    rl.question("enter city : ", (cityName)=>{
-        console.log(cityName)
-        for (let i = 0; i  <  cities.length;i++);
-        let current = cities[i] ;
-        if (current.name === cityName){
-            console.log('you chose :' + current.name);
-        }else{
-            console.log('Bye !!!')
+
+function func() {
+  fs.readFile('input.txt', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return;
+    }
+
+    cities = JSON.parse(data);
+    console.log(cities);
+
+    rl.question('enter city : ', (cityName) => {
+      console.log(cityName);
+      for (let i = 0; i < cities.length; i++) {
+        let current = cities[i];
+        if (current.name === cityName) {
+          console.log('you chose :' + current.name);
+          break; 
+        } else {
+          console.log('Bye !!!');
         }
-    })
-    
+      }
+
+      rl.close(); 
+    });
+  });
 }
 
+
+func();
